d512cfd6b52761b14c0992d3731a44bbd1a2f84c
taterntots <matthew.a.weidner@gmail.com>
2019-11-04 15:57:10 -0500

    got a timer to work for stretch, but only in seconds. Also tried to get the quarter to implement, but variables are not importing properly between files.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,6 +7,31 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(7);
   const [awayScore, setAwayScore] = useState(24);
+  const [quarter, setQuarter] = useState(1);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    document.title = 'Time: ' + seconds;
+    let interval = null;
+    if(isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    }else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
 
   return (
     <div className="container">
@@ -19,7 +44,7 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{seconds}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
@@ -35,7 +60,16 @@ function App() {
         </div>
         <div className="awayButtons">
           <button onClick= {() => setAwayScore(awayScore + 7)} className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick= {() => setAwayScore(awayScore + 3)}className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick= {() => setAwayScore(awayScore + 3)} className="awayButtons__fieldGoal">Away Field Goal</button>
+        </div>
+        <div className="quarterButton">
+          <button onClick= {() => setQuarter(quarter + 1)}>Quarter</button>
+        </div>
+        <div className="timerButton">
+          <button className= {() => isActive ? 'active' : 'inactive'} onClick={toggle}>{isActive ? 'Pause' : 'Start'}</button>
+        </div>
+        <div className="resetTimerButton">
+          <button className= "button" onClick={reset}>Reset</button>
         </div>
       </section>
     </div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./App.css";
+import quarter from "./App.js";
 
 const BottomRow = () => {
+
+  // const [quarter, setQuarter] = useState(1);
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +22,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
       </div>
     </div>
   );
